b92d1c6cad5e073af13f5fa33feffc8eb9e6df10
LuisYtanare <gricert07alexander@gmail.com>
2021-11-01 12:52:12 -0400

    Crear App utilizando CLI

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React from 'react'
+
+export const App = () => {
+  return (
+    <div>
+      <h1>HElooooooo!</h1>
+    </div>
+  )
+}
+
+
+export default App;
